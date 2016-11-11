d009c4118dd9ea60c849f1cd02aad165e9213ae5
Lesley Andrez <lesley.andrez@gmail.com>
2016-11-11 21:15:12 -0200

    Usando o objeto global localStorage diretamente

--- src/js/model/todoListStore.js
+++ src/js/model/todoListStore.js
@@ -1,23 +1,21 @@
-const store = localStorage;
-
 export default class TodoListStore {
 
   constructor (idStore) {
     this.idStore = idStore;
-    if(store.getItem(this.idStore) == null){
-      store.setItem(this.idStore, '[]');
+    if(localStorage.getItem(this.idStore) == null){
+      localStorage.setItem(this.idStore, '[]');
     }
   }
 
   getStore () {
-    return JSON.parse(store.getItem(this.idStore));
+    return JSON.parse(localStorage.getItem(this.idStore));
   }
 
   updateStore (itens) {
-    store.setItem(this.idStore, JSON.stringify(itens));
+    localStorage.setItem(this.idStore, JSON.stringify(itens));
   }
 
   clearStore () {
-    store.setItem(this.idStore, '');
+    localStorage.setItem(this.idStore, '');
   }
 }
